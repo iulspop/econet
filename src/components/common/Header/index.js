0c8952f9d329093d5d2124622baf1e40deb8e9a6
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-02-14 19:24:11 -0500

    begin make nav responsive

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -6,34 +6,55 @@ import { theme } from "../../theme"
 export const Header = () => {
   return (
     <Header_Container>
-      <LogoLink href={"#"}>
-        <Logo
-          src={"assets/logo.png"}
-          alt={"Nettoyage Éconet. Nettoyage écologique de tapis et de meubles"}
-        ></Logo>
-      </LogoLink>
-      <Nav>
-        <Link href={"#"}>. Accueil .</Link>
-        <Link href={"#"}>. À Propos .</Link>
-        <Link href={"#"}>. Services .</Link>
-        <Link2 href={"#"}>. Estimation .</Link2>
-        <Link href={"#"}>. Contact .</Link>
-        <ToggleLanguage>EN/FR</ToggleLanguage>
-      </Nav>
+      <Header_Child>
+        <LogoLink href={"#"}>
+          <picture>
+            <source media="(max-width: 1300px)" srcset="assets/logo-slim.png" />
+            <Logo
+              src={"assets/logo.png"}
+              alt={
+                "Nettoyage Éconet. Nettoyage écologique de tapis et de meubles"
+              }
+            />
+          </picture>
+        </LogoLink>
+        <Nav>
+          <Link href={"#"}>. Accueil .</Link>
+          <Link href={"#"}>. À Propos .</Link>
+          <Link href={"#"}>. Services .</Link>
+          <Link2 href={"#"}>. Estimation .</Link2>
+          <Link href={"#"}>. Contact .</Link>
+          <Link3 href={"#"}>514-555-5555</Link3>
+          <Link4 href={"#"}>
+            <ToggleBold bold>EN</ToggleBold> / <ToggleBold>FR</ToggleBold>
+          </Link4>
+        </Nav>
+      </Header_Child>
     </Header_Container>
   )
 }
 
-const Header_Container = styled.header`
+const Header_Container = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const Header_Child = styled.header`
+  max-width: 1800px;
   width: 100%;
   height: 130px;
   padding: 0 6rem;
   display: flex;
   justify-content: space-between;
+
+  @media only screen and (max-width: 1300px) {
+    height: 100px;
+    padding: 0 3rem;
+  }
 `
 
 const Nav = styled.nav`
-  flex-basis: 800px;
+  flex-basis: 900px;
   display: flex;
   justify-content: space-between;
 `
@@ -45,6 +66,10 @@ const LogoLink = styled.a`
 
 const Logo = styled.img`
   height: 100px;
+
+  @media only screen and (max-width: 1300px) {
+    height: 50px;
+  }
 `
 
 const Link = styled.a`
@@ -67,6 +92,15 @@ const Link2 = styled(Link)`
   width: 130px;
 `
 
-const ToggleLanguage = styled.button`
-  ${buttonReset}
+const Link3 = styled(Link)`
+  width: 150px;
+  font-weight: 700;
+`
+
+const Link4 = styled(Link)`
+  width: 80px;
+`
+
+const ToggleBold = styled.span`
+  font-weight: ${props => (props.bold ? "900" : "400")};
 `
