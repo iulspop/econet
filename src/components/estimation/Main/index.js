d78e74e03e0c366b37ae6d8fbca370c4eb665565
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-03-02 18:15:22 -0500

    change estimation main spacing

--- src/components/estimation/Main/index.js
+++ src/components/estimation/Main/index.js
@@ -16,41 +16,39 @@ export const Main = () => {
 
   return (
     <StyledMain>
-      <Container>
+      <Flexbox>
         <DecorativeLine
           src={"/assets/decorative-horizontal-line.png"}
           alt={""}
         />
         <MainHeading>Estimation</MainHeading>
-      </Container>
+        <SecondaryHeading shownForm={shownForm}>
+          Afin de bien vous servir,
+          <br /> choisissez le service vous cherchez
+        </SecondaryHeading>
+        <Container shownForm={shownForm}>
+          <DecoratedButton
+            clickAction={showResidentialForm}
+            active={shownForm === "residential"}
+            ariaLabel="open résidential services form"
+            left
+          >
+            Résidentiel
+          </DecoratedButton>
+          <DecoratedButton
+            clickAction={showCommercialForm}
+            active={shownForm === "commercial"}
+            ariaLabel="open commercial services form"
+          >
+            Commercial
+          </DecoratedButton>
+        </Container>
+      </Flexbox>
 
-      <SecondaryHeading shownForm={shownForm}>
-        Afin de bien vous servir, veuillez nous indiquer quel type de service
-        vous cherchez.
-      </SecondaryHeading>
-
-      <Container>
-        <DecoratedButton
-          clickAction={showResidentialForm}
-          active={shownForm === "residential"}
-          ariaLabel="open résidential services form"
-          left
-        >
-          Résidentiel
-        </DecoratedButton>
-        <DecoratedButton
-          clickAction={showCommercialForm}
-          active={shownForm === "commercial"}
-          ariaLabel="open commercial services form"
-        >
-          Commercial
-        </DecoratedButton>
-      </Container>
-
-      <Container>
+      <div>
         <Form shownForm={shownForm} formName="commercial-estimation" />
         <Form shownForm={shownForm} formName="residential-estimation" />
-      </Container>
+      </div>
     </StyledMain>
   )
 }
@@ -62,13 +60,20 @@ const StyledMain = styled.main`
   align-items: center;
   min-height: 100vh;
 `
+const Flexbox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 const Container = styled.div`
-  display: ${props => (props.shownForm ? "none" : "block")};
+  display: inline-block;
+  margin-top: ${props => (props.shownForm ? "2rem" : "4.7rem")};
 `
 
 const SecondaryHeading = styled.h2`
   display: ${props => (props.shownForm === "" ? "block" : "none")};
+  text-align: center;
 `
 
 const DecorativeLine = styled.img`
