202da14a18953b6095188aadb1a252dd3a3c9117
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-03-03 16:01:56 -0500

    add 30min increments to hour select

--- src/components/estimation/Main/Form/Visit/index.js
+++ src/components/estimation/Main/Form/Visit/index.js
@@ -32,16 +32,27 @@ export const Visit = ({ formStep, backwardFormStep }) => {
           Heure Préférée
           <Select name="time">
             <option value="8 AM">8 AM</option>
+            <option value="8:30 AM">8:30 AM</option>
             <option value="9 AM">9 AM</option>
+            <option value="9:30 AM">9:30 AM</option>
             <option value="10 AM">10 AM</option>
+            <option value="10:30 AM">10:30 AM</option>
             <option value="11 AM">11 AM</option>
+            <option value="11:30 AM">11:30 AM</option>
             <option value="12 AM">12 AM</option>
+            <option value="12:30 AM">12:30 AM</option>
             <option value="1 PM">1 PM</option>
+            <option value="1:30 PM">1:30 PM</option>
             <option value="2 PM">2 PM</option>
+            <option value="2:30 PM">2:30 PM</option>
             <option value="3 PM">3 PM</option>
+            <option value="3:30 PM">3:30 PM</option>
             <option value="4 PM">4 PM</option>
+            <option value="4:30 PM">4:30 PM</option>
             <option value="5 PM">5 PM</option>
+            <option value="5:30 PM">5:30 PM</option>
             <option value="6 PM">6 PM</option>
+            <option value="6:30 PM">6:30 PM</option>
           </Select>
         </Label>
         <RadioGroup>
