a749ad65dd36849a814f2125eddba60a4d52c085
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-03-12 18:16:40 -0400

    add contact info box

--- src/components/Estimation/index.js
+++ src/components/Estimation/index.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 import { useTranslation } from "react-i18next"
 import { DecoratedButton } from "../common"
 import { Form } from "./Form"
+import { Contact } from "./Form/Contact"
 
 export const Estimation = () => {
   const { t } = useTranslation("Estimation")
@@ -52,6 +53,27 @@ export const Estimation = () => {
       <div>
         <Form shownForm={shownForm} formName="commercial" />
         <Form shownForm={shownForm} formName="residential" />
+        <ContactInfoBox>
+          <h3>Coordonnées</h3>
+          <p>
+            <strong>Téléphone:</strong>
+            <a href="tel:514-874-9488">514-874-9488</a>
+          </p>
+          <p>
+            <strong>Heures de Travail</strong>
+            <br></br>
+            Lundi - Vendredi<br></br>
+            9AM - 6PM
+          </p>
+          <p>
+            Nous desservons Montréal, Laval, Longueuil<br></br>& les régions
+            avoisinantes.
+          </p>
+          <address>
+            2763 Rue French, local 300<br></br>
+            Montréal, QC H1L 4S2
+          </address>
+        </ContactInfoBox>
       </div>
     </StyledMain>
   )
@@ -122,4 +144,13 @@ const SecondaryHeading = styled.h2`
   @media only screen and ${props => props.theme.media.phone} {
     font-size: 2rem;
   }
+`
+
+const ContactInfoBox = styled.aside`
+  width: 100%;
+  margin: 4rem 0;
+  padding: 2rem 1.5rem;
+  background-color: white;
+  border-radius: 1px;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
 `
