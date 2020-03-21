d57e3162c42105e89babb1cd33b21f7fe820b3c0
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-03-21 19:29:38 -0400

    refactor button style position

--- src/components/common/DecoratedButton/Button/index.js
+++ src/components/common/DecoratedButton/Button/index.js
@@ -1,6 +1,6 @@
 import React from "react"
-import styled from "styled-components"
-import { buttonReset, linkReset, buttonStyle } from "../../../../mixins"
+import styled, { css } from "styled-components"
+import { buttonReset, linkReset } from "../../../../mixins"
 
 export const Button = ({
   clickAction,
@@ -33,6 +33,61 @@ export const Button = ({
     </React.Fragment>
   )
 }
+
+const buttonStyle = css`
+  padding: 2rem 0;
+  margin: 0 5rem;
+  position: relative;
+
+  font-size: 3.5rem;
+  font-weight: 700;
+  color: ${props => (props.inactive ? "gray" : props.theme.colors.primary)};
+  text-transform: uppercase;
+  text-align: center;
+
+  cursor: pointer;
+
+  @media only screen and ${props => props.theme.media.tabPort} {
+    font-size: 2.3rem;
+    padding: 1.5rem 0;
+    margin: 0 3rem;
+  }
+
+  @media only screen and ${props => props.theme.media.phone} {
+    display: block;
+    width: 150px;
+    margin: 2rem auto;
+    margin-bottom: 3rem;
+  }
+
+  &::before {
+    width: 130%;
+    height: 3px;
+    content: "";
+    background: radial-gradient(
+      circle at center,
+      ${props => (props.inactive ? "gray" : props.theme.colors.primary)},
+      transparent
+    );
+    position: absolute;
+    top: 0;
+    left: -15%;
+  }
+
+  &::after {
+    width: 130%;
+    height: 3px;
+    content: "";
+    background: radial-gradient(
+      circle at center,
+      ${props => (props.inactive ? "gray" : props.theme.colors.primary)},
+      transparent
+    );
+    position: absolute;
+    top: 100%;
+    left: -15%;
+  }
+`
 
 const StyledButton = styled.button`
   ${buttonReset}
