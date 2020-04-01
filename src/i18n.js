a1dd9481ccf5ebe2d0738cb95f4d4c5beda79c29
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-04-01 18:43:51 -0400

    remove browser lang detector from i18next

--- src/i18n.js
+++ src/i18n.js
@@ -1,15 +1,11 @@
 import i18n from "i18next"
 import Backend from "i18next-xhr-backend"
-import LanguageDetector from "i18next-browser-languagedetector"
 import { initReactI18next } from "react-i18next"
 
 i18n
   // load translation using xhr -> see /src/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
   // learn more: https://github.com/i18next/i18next-xhr-backend
   .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
@@ -19,31 +15,17 @@ i18n
       loadPath: "/locales/{{lng}}/{{ns}}.json",
     },
 
-    detection: {
-      order: [
-        "querystring",
-        "path",
-        "htmlTag",
-        "navigator",
-        "cookie",
-        "localStorage",
-      ],
-      lookupQuerystring: "lng",
-      checkWhitelist: true,
-    },
-
     lng: "fr",
     fallbackLng: "fr",
     whitelist: ["en", "fr"],
     debug: false,
-    ns: ["Header", "Banner"],
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
 
     react: {
-      useSuspense: false,
+      useSuspense: false, // causes crash with SSR
     },
   })
 
