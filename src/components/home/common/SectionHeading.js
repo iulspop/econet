9b4a8a3cb403825ca665274c2fafc5c23021404c
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-04-04 16:57:38 -0400

    reduce SectionHeading line height

--- src/components/home/common/SectionHeading.js
+++ src/components/home/common/SectionHeading.js
@@ -11,6 +11,7 @@ export const SectionHeading = ({ children, className, id }) => {
 
 const StyledSectionHeading = styled.h3`
   font-size: 5rem;
+  line-height: 1.3;
   margin-bottom: 1rem;
 
   @media only screen and ${props => props.theme.media.tabPort} {
