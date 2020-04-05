351ddab2e4ba99d331833be2d0e93767be8003fa
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-04-05 11:56:07 -0400

    add gatsby plugin sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,12 @@
  */
 
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-netlify`],
+  siteMetadata: {
+    siteUrl: `https://nettoyageeconet.netlify.com`,
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-netlify`,
+    `gatsby-plugin-sitemap`,
+  ],
 }
